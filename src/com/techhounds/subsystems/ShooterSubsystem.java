6244acced9ab5782158ce94dfa18ec41f8476177
claytondetke <cjdet_000@10.35.48.35>
2016-02-10 16:10:07 -0500

    Finished ShooterSubsystem with placeholders in RobotMap

--- src/com/techhounds/subsystems/ShooterSubsystem.java
+++ src/com/techhounds/subsystems/ShooterSubsystem.java
@@ -1,10 +1,43 @@
 package com.techhounds.subsystems;
 
+import com.techhounds.RobotMap;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class ShooterSubsystem {
 
 	private static ShooterSubsystem instance;
+	private Spark shoot;
+	private boolean isInverted;
 	
 	private ShooterSubsystem() {
+		shoot = new Spark(RobotMap.Shooter.SHOOTER_MOTOR);
+		isInverted = RobotMap.Shooter.SHOOTER_IS_INVERTED;
+	}
+	
+	public void setPower(double power){
+		power = Math.min((Math.max(power, -1)), 1);
+		if(isInverted){
+			shoot.set(-power);
+		}else{
+			shoot.set(power);
+		}
+	}
+	
+	public void stopPower(){
+		shoot.set(0);
+	}
+	
+	public double getPower(){
+		return shoot.get();
+	}
+	
+	public void UpdateSmartDashboard(){
+		SmartDashboard.putNumber("Shooter_Power", getPower());
+	}
+	
+	public void initDefaultCommand(){
 	}
 	
 	public static ShooterSubsystem getInstance() {
