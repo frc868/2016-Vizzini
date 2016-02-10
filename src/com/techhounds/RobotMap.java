e852852616db02ebf820a1a64cec0469fb9dfee8
claytondetke <cjdet_000@10.35.48.35>
2016-02-09 20:22:35 -0500

    Converted RobotMap from interface to class

--- src/com/techhounds/RobotMap.java
+++ src/com/techhounds/RobotMap.java
@@ -1,20 +1,5 @@
 package com.techhounds;
 
-public interface RobotMap {
+public class RobotMap {
 	
-	interface Drive {
-		// TODO: Add Drive Ports
-	}
-	
-	interface Collector {
-		// TODO: Add Collector Ports
-	}
-	
-	interface Shooter {
-		// TODO: Add Shooter Ports
-	}
-	
-	interface Elevator {
-		// TODO: Add Elevator Ports
-	}
 }
