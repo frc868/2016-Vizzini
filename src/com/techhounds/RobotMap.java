91629b0698491f68a95a8fe5b2356f18b3e55dd7
jacob <jacob@10.35.44.193>
2016-02-10 17:29:42 -0500

    Work on collector angler subsystem

--- src/com/techhounds/RobotMap.java
+++ src/com/techhounds/RobotMap.java
@@ -2,6 +2,7 @@ package com.techhounds;
 
 public class RobotMap {
 	public static final int COLLECTOR_MOTOR = -1;
+	public static final int COLLECTOR_ANGLER = -1;
 	
 	public static class Shooter{
 		public static final int SHOOTER_MOTOR = -1;
--- src/com/techhounds/subsystems/CollectorAnglerSubsystem.java
+++ src/com/techhounds/subsystems/CollectorAnglerSubsystem.java
@@ -1,5 +1,8 @@
 package com.techhounds.subsystems;
 
+import com.techhounds.RobotMap;
+
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -8,13 +11,25 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class CollectorAnglerSubsystem extends Subsystem {
 	
 	private static CollectorAnglerSubsystem instance;
+	
+	private CANTalon angler;
     
+	public CollectorAnglerSubsystem(CANTalon angler) {
+		this.angler = angler;
+		this.angler.enableForwardSoftLimit(true);
+		this.angler.enableReverseSoftLimit(true);
+	}
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+	public void setPower(double power) {
+		power = Math.min(power, 1);
+		power = Math.max(power, -1);
+	}
 	
 	public static CollectorAnglerSubsystem getInstance(){
 		if(instance == null){
-			instance = new CollectorAnglerSubsystem();
+			CANTalon x = new CANTalon(RobotMap.COLLECTOR_ANGLER);
+			instance = new CollectorAnglerSubsystem(x);
 		}
 		return instance;
 	}
