1f3d127920a4308851f035aea20922c5fd70c00c
caberbot <caberbot2@gmail.com>
2016-02-11 17:18:16 -0500

    Worked on DriveSubsystem

--- src/com/techhounds/subsystems/DriveSubsystem.java
+++ src/com/techhounds/subsystems/DriveSubsystem.java
@@ -1,5 +1,7 @@
 package com.techhounds.subsystems;
 
+import edu.wpi.first.wpilibj.Spark;
+
 public class DriveSubsystem{
 
 	// TODO: Add SpeedControllers for Left and Right Drive
@@ -9,11 +11,22 @@ public class DriveSubsystem{
 	// TODO: Add Drive Distance PID Controller
 	// TODO: Add Gyro Turn PID Controller
 	
+	private Spark left;
+	private Spark right;
+	
 	private static DriveSubsystem instance;
 	
+	private DriveSubsystem(Spark l, Spark r) {
+		left = l;
+		right = r;
+	}
+	
 	public static DriveSubsystem getInstance() {
-		if(instance == null)
-			instance = new DriveSubsystem();
+		if(instance == null) {
+			Spark l = new Spark(-1);
+			Spark r = new Spark(-1);
+			instance = new DriveSubsystem(l, r);
+		}
 		return instance;
 	}
 	
