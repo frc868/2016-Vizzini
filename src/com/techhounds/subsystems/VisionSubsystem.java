512815ec56d3a1b1c4d903e1fb60da4f1151b804
claytondetke <cjdet_000@10.35.48.35>
2016-02-11 19:30:48 -0500

    Added basic get methods and updateSD for Vision subsystem, and placeholder commands for Angler and Collector

--- src/com/techhounds/subsystems/VisionSubsystem.java
+++ src/com/techhounds/subsystems/VisionSubsystem.java
@@ -1,8 +1,18 @@
 package com.techhounds.subsystems;
 
+import edu.wpi.first.wpilibj.networktables.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class VisionSubsystem{
 
 	private static VisionSubsystem instance;
+	private NetworkTable netTable;
+	
+	public VisionSubsystem(){
+		NetworkTable.setServerMode();
+		NetworkTable.initialize();
+		netTable = NetworkTable.getTable("SmartDashboard");
+	}
 	
 	public static VisionSubsystem getInstance() {
 		if(instance == null)
@@ -10,7 +20,22 @@ public class VisionSubsystem{
 		return instance;
 	}
 	
+	public double getDistanceFromBase(){
+		return netTable.getNumber("DistanceToBase", 0);
+	}
+	
+	public double getDistanceToTarget(){
+		return netTable.getNumber("DistanceToTarget", 0);
+	}
+	
+	public double getAngle(){
+		return netTable.getNumber("OffCenterDegreesX", 0);
+	}
+	
 	public void updateSmartDashboard() {
+		SmartDashboard.putNumber("Distance_From_Base", getDistanceFromBase());
+		SmartDashboard.putNumber("Distance_To_Target", getDistanceToTarget());
+		SmartDashboard.putNumber("Degree_Off_Center", getAngle());
 		// TODO: Add Things That Need to be Sent to SmartDashboard for Information
 	}
 
