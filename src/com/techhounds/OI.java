5ec9ee759ee4ace1036476ef051b550bb995eee2
calvinh <calvinh@localhost.localdomain>
2016-02-22 11:59:52 -0500

    Switched OI to use Identical controls for Operator and Driver

--- src/com/techhounds/OI.java
+++ src/com/techhounds/OI.java
@@ -82,7 +82,7 @@ public class OI {
 	
 	private void setup() {
 		if(Robot.oneControllerMode) {
-			setupController();
+			setUpController(driver);
 		} else {
 			setupDriver();
 			setupOperator();
@@ -102,14 +102,17 @@ public class OI {
 	 * Gets the Driver Controller Ready with its Buttons
 	 */
 	public void setupDriver() {
-		setupController();
+		setUpController(driver);
 	}
 	
 	/**
 	 * Gets the Operator Controller Ready with its Buttons
 	 */
 	public void setupOperator() {
-		operator.getButton(opCollectIn).
+		setUpController(operator);
+		
+		
+		/*operator.getButton(opCollectIn).
 		whenPressed(new SetCollectorPower(RobotMap.Collector.inPower)).
 		whenReleased(new SetCollectorPower());
 		
@@ -124,53 +127,47 @@ public class OI {
 		operator.getButton(opShooterOff).whenPressed(new SetShooterSpeed(0));
 		
 		operator.getButton(opShooterUp).whenPressed(new IncrementShooterSpeed(1));
-		operator.getButton(opShooterDown).whenPressed(new IncrementShooterSpeed(-1));
+		operator.getButton(opShooterDown).whenPressed(new IncrementShooterSpeed(-1));*/
 	}
 	
 	/**
 	 * Setup Single Controller Control
 	 */
-	public void setupController() {
+	public void setUpController(ControllerMap controller) {
 		
-		driver.getButton(startCollector)
+		controller.getButton(startCollector)
 			.whenPressed(new SetCollectorPower(RobotMap.Collector.inPower))
 			.whenReleased(new SetCollectorPower());
 		
-		driver.getButton(stopCollector)
+		controller.getButton(stopCollector)
 			.whenPressed(new SetCollectorPower(RobotMap.Collector.outPower))
 			.whenReleased(new SetCollectorPower());
 		
-		driver.getButton(angleUp)
+		controller.getButton(angleUp)
 			.whenPressed(new SetStateUp());
 		
-		driver.getButton(angleDown)
+		controller.getButton(angleDown)
 			.whenPressed(new SetStateDown());
 
-		driver.getButton(upShooterSpeed)
-			.whenPressed(new SetShooterPower(.1, true));
+		controller.getButton(upShooterSpeed)
+			.whenPressed(new IncrementShooterSpeed(1));
 		
-		driver.getButton(downShooterSpeed)
-			.whenPressed(new SetShooterPower(-.1, true));
+		controller.getButton(downShooterSpeed)
+			.whenPressed(new IncrementShooterSpeed(1));
 		
-		driver.getButton(stopShooter)
+		controller.getButton(stopShooter)
 			.whenPressed(new SetShooterPower());
 		
-		driver.getButton(startShooter)
+		controller.getButton(startShooter)
 			.whenPressed(new SetShooterSpeed(69));
 		
-		driver.getButton(fireShooter)
+		controller.getButton(fireShooter)
 			.whenPressed(new Fire());
-/*		
-		driver.getButton(collectAngler)
-			.whenPressed(new SetStateUp());
-		
-		driver.getButton(collectDefenses)
-			.whenPressed(new SetStateDown());
-*/		
-		driver.getButton(toggleDrive)
+
+		controller.getButton(toggleDrive)
 			.whenPressed(new ToggleDriveDirection());
 	
-		driver.getButton(visionTarget)
+		controller.getButton(visionTarget)
 			.whenPressed(new VisionRotateToTarget());
 			
 	}
