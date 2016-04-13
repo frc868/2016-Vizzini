dade5066e1f25876cba8d1ac89cd3040bc006e8e
Calvin Henry <developer@10.35.16.221>
2016-04-13 12:37:21 -0400

    Added Drive Distance Straight (using Gyro to keep aligned)

---
+++ src/com/techhounds/commands/drive/DriveDistanceStraight.java
@@ -0,0 +1,170 @@
+package com.techhounds.commands.drive;
+
+import com.techhounds.Robot;
+import com.techhounds.RobotMap;
+import com.techhounds.subsystems.DriveSubsystem;
+import com.techhounds.subsystems.GyroSubsystem;
+
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class DriveDistanceStraight extends Command implements PIDSource, PIDOutput {
+
+	private DriveSubsystem drive;
+	private GyroSubsystem gyro;
+	private double targetDist;
+	private PIDController pid;
+	private double lastPower;
+	private double minPower;
+	private double initAngle;
+	private boolean pidOnTarget;
+	private Boolean lightSensorReading;
+	
+	private Double timeOut;
+
+	public DriveDistanceStraight(double dist) {
+		this(dist, 1);
+	}
+	public DriveDistanceStraight(double dist, boolean waitForOnDefense){
+		this(dist);
+		lightSensorReading = waitForOnDefense;
+	}
+
+	public DriveDistanceStraight() {
+		this(30, .4);
+	}
+
+	public DriveDistanceStraight(double dist, double max, double min) {
+		drive = DriveSubsystem.getInstance();
+		gyro = GyroSubsystem.getInstance();
+		requires(drive);
+		pid = new PIDController(.025, 0, .004, this, this);
+		pid.setOutputRange(-Math.abs(max), Math.abs(max));
+		pid.setAbsoluteTolerance(1);
+		minPower = min;
+		this.targetDist = dist;
+		//SmartDashboard.putData("Drive distance pid", pid);
+		//SmartDashboard.putNumber("Power to Drive", 0.3);
+		//SmartDashboard.putNumber("Min Power To Drive", min);
+	}
+	
+	public DriveDistanceStraight(double dist, double max, double min, double timeOut) {
+		this(dist, max, min);
+		this.timeOut = timeOut;
+	}
+
+	public DriveDistanceStraight(double dist, double max) {
+		this(dist, max, RobotMap.DriveTrain.MIN_STRAIGHT_POWER);
+	}
+
+	@Override
+	protected void initialize() {
+		// minPower = SmartDashboard.getNumber("Min Power To Drive", .2);
+		initAngle = gyro.getRotation();
+		double curDist = drive.countsToDist(drive.getAvgDistance());
+		lastPower = 0;
+		pid.setSetpoint(targetDist + curDist);
+		pid.enable();
+
+	}
+
+	@Override
+	protected void execute() {
+		SmartDashboard.putNumber("Drive Error", pid.getError());
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected boolean isFinished() {
+		if(timeOut != null){
+			if(timeSinceInitialized() > timeOut){
+				return true;
+			}
+		}
+		if(lightSensorReading != null){
+			return lightSensorReading == drive.onDefense();
+		}
+		return pidOnTarget = pid.onTarget();
+	}
+
+	@Override
+	protected void end() {
+		pid.disable();
+		drive.setPower(0, 0);
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void interrupted() {
+		end();
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pidWrite(double output) {
+		double pidOutput = output;
+		double difference = output - lastPower;
+		if (difference > .1) {
+			output = lastPower + .1;
+		} else if (difference < -.1) {
+			output = lastPower - .1;
+		}
+
+		//if(output < minPower && output > -minPower)
+		//	output = output > 0 ? minPower : -minPower;
+
+		// If PID wants us to move negative, make sure we are at least negative
+		// min power same for positive condition
+		//if ((pidOutput < 0) && (output > -minPower)) {
+		//	output = -minPower;
+		//} else if ((pidOutput > 0) && (output < minPower)) {
+		//	output = minPower;
+		//}
+		
+		//output = output > 0 ? minPower : -minPower;
+
+		if (pidOnTarget)
+			output = 0;
+		
+		double currAngle = gyro.getRotation() - initAngle;
+		double offset;
+		
+		if(currAngle > 9.11346) {
+			offset = .21;
+		} else if(currAngle < -9.01606) {
+			offset = -.21;
+		} else {
+			// A power between -.21 and .21
+			offset = -0.000136434109 * Math.pow(currAngle, 3) + 0.0000199335555 * Math.pow(currAngle, 2)
+				+ 0.03363132991 * (currAngle);
+		}
+		
+		lastPower = output;
+		drive.setPower(Robot.rangeCheck(output + offset), Robot.rangeCheck(output - offset));
+
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPIDSourceType(PIDSourceType pidSource) {
+	}
+
+	@Override
+	public PIDSourceType getPIDSourceType() {
+		return PIDSourceType.kDisplacement;
+	}
+
+	@Override
+	public double pidGet() {
+		return drive.countsToDist(drive.getAvgDistance());
+	}
+
+}
