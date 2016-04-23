773166e798cb87151b19789587f38ce149a2ea2e
Calvin Henry <developer@10.35.16.221>
2016-04-23 19:07:06 -0400

    Added Check for Tilt Pattern and a Drive with Tilt Pattern (Not implemented in actual code yet)

---
+++ src/com/techhounds/commands/drive/DriveUntilTiltPattern.java
@@ -0,0 +1,41 @@
+package com.techhounds.commands.drive;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class DriveUntilTiltPattern extends CommandGroup {
+
+	private Command tiltCommand;
+	
+	public DriveUntilTiltPattern(double speed) {
+		
+		if(speed > 0) {
+			addParallel(tiltCommand = new CheckForTiltPattern(CheckForTiltPattern.DEFENSE_CROSS_FORWARD, 
+					CheckForTiltPattern.DEFENSE_CROSS_TIMEOUTS, 5));
+		} else {
+			addParallel(tiltCommand = new CheckForTiltPattern(CheckForTiltPattern.DEFENSE_CROSS_BACKWARD, 
+					CheckForTiltPattern.DEFENSE_CROSS_TIMEOUTS, 5));
+		}
+		
+		addParallel(new DriveWithSpeed(speed));
+	}
+	
+	public DriveUntilTiltPattern(double speed, CheckForTiltPattern.Motion [] motion, Double [] timeout) {
+		addParallel(tiltCommand = new CheckForTiltPattern(motion, timeout, 5));
+		addParallel(new DriveWithSpeed(speed));
+	}
+	
+	public DriveUntilTiltPattern(double speed, CheckForTiltPattern.Motion [] motion, Double [] timeout, double angleThreshold) {
+		addParallel(tiltCommand = new CheckForTiltPattern(motion, timeout, angleThreshold));
+		addParallel(new DriveWithSpeed(speed));
+	}
+	
+	@Override
+	public boolean isFinished() {
+		if(!tiltCommand.isRunning()) {
+			return true;
+		} else {
+			return super.isFinished();
+		}
+	}
+}
